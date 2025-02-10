1addc8f7a9be7ee9c2514ea82b865c3642ec0faf
Tejashree1631 <tejashreethamby@gmail.com>
2025-02-10 23:17:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿document.getElementById("yesButton").addEventListener("click", function() {
+  // Show confetti when 'Yes' is clicked
+  confetti();
+
+  // Optionally, show a message after confetti
+  alert("Yay! Thank you for being my Valentine!");
+});
+
+document.getElementById("noButton").addEventListener("click", function() {
+  alert("Oh no! Ur gor no option its just yes.");
+});
